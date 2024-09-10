9b4988848302811d4514c196a4cef641e9a124a7
Yamila Silva <lic.silvayasin@gmail.com>
2024-09-10 16:44:34 +0100

    EntregaFinal

--- src/Firebase/dbConnection.js
+++ src/Firebase/dbConnection.js
@@ -1,36 +1,17 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// export const db = getFirestore(app)
-
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyAhymq7CsExX7FgxQHU3yP8Mg-VUjAL6Ww",
-authDomain: "mr-calcetin.firebaseapp.com",
-projectId: "mr-calcetin",
-storageBucket: "mr-calcetin.appspot.com",
-messagingSenderId: "967814412842",
-appId: "1:967814412842:web:2bd0acd5262d0d45402153"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-// Initialize Firebase
+console.log(import.meta.env.VITE_API_KEY)
+
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app)
+export const db = getFirestore(app);
